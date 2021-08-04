eb69cd502493dd54ee48004b0324d7577bcba98a
caoqiming <caoqiming@bytedance.com>
2021-08-04 11:27:28 +0800

    serverok

--- RGB565.cpp
+++ RGB565.cpp
@@ -11,31 +11,25 @@ using boost::asio::ip::udp;
 using std::string;
 using std::cout;
 using std::endl;
-#define TARGETH 230
-#define TARGETW 230
+#define TARGETH 240
+#define TARGETW 240
+#define BufferSize 3840
+#define PackageNums 15
+// 240*240/15=3840 分15个包发
 
-void make_command(string c, char* data){
+void make_command(string c, char* data, int index=0){
 	if(data==nullptr){
 		cout<<"make_command get nullptr"<<endl;
 		return;
 	}
-	if(c=="pic1"){
-		*data=0x00;// 第一部分数据
-	}else if(c=="pic2"){
-		*data=0x01;// 第二部分数据
-	}else if(c=="pic3"){
-		*data=0x02;// 第三部分数据
+	if(c=="pic"){
+		*data=static_cast<char>(index);// 第一部分数据
+	}else if(c=="pic_size"){
+		*data=static_cast<char>(15);// 图片大小 显示x 显示y
 	}
-	else if(c=="pic_size"){
-		*data=0x03;// 图片大小 显示x 显示y
-	}
-	
 }
 
-void get_pic_size(const int &size,int &size_1,int &size_3){ //根据图片大小获取图片每个包的大小，一个图片分三个包发送,size2和size1相同
-	size_1 = size/3;
-	size_3 = size - 2*size_1;
-}
+
 
 void rgb565(cv::Mat &image_resized,uint16_t *&data ,size_t &data_size){
     int target_w=image_resized.cols;
@@ -48,7 +42,7 @@ void rgb565(cv::Mat &image_resized,uint16_t *&data ,size_t &data_size){
         uchar* p = image_resized.ptr(row); 
         for(int col = 0; col < image_resized.cols*3; ) { //B,G,R
             //B 8-3=5
-            *data_p |= *(p+2)>>3; 
+            *data_p |= *(p+2)>>3;
             *data_p <<=6;
             //G 8-2=6
             *data_p |= *(p+1)>>2;
@@ -60,7 +54,7 @@ void rgb565(cv::Mat &image_resized,uint16_t *&data ,size_t &data_size){
             p+=3;
             ++data_p;
         }
-    }   
+    }
 }
 
 void reshape(cv::Mat &image, cv::Mat &image_resized){
@@ -138,6 +132,8 @@ int main(int argc, char* argv[]){
 
     cv::Mat image;
     image = cv::imread("cat.jpeg");
+    // cv::imshow("jojo",image);
+    // cv::waitKey(0);
     cv::Mat image_resized;
     reshape(image,image_resized);
 
@@ -146,42 +142,24 @@ int main(int argc, char* argv[]){
     rgb565(image_resized,data,data_size);
 
     size_t data_size_c = data_size * 2;
-    uchar* data_c = (uchar*)data;
+    char* data_c = (char*)data;
 
     int a;
-
+    cout<<"input:"<<endl;
     std::cin >> a;
 	boost::asio::io_service io_service;
-	UDPClient client(io_service, "192.168.4.1", "2333");
+	UDPClient client(io_service, "127.0.0.1", "2333");
 
-    int size_1, size_3;
-    get_pic_size(data_size_c, size_1, size_3);
-    cout << "size0:" << size_1 << " size_2:" << size_3 << endl;
-    char* data_1 = new char[size_1 + 1];
-    char* data_2 = new char[size_1 + 1];
-    char* data_3 = new char[size_3 + 1];
-    make_command("pic1",data_1);
-    make_command("pic2",data_2);
-    make_command("pic3",data_3);
-    memcpy(data_1 + 1, data, size_1);
-    memcpy(data_2 + 1, data+size_1, size_1);
-    memcpy(data_3 + 1, data+2*size_1, size_3);
+    char* data_buffer = new char[BufferSize + 1];
+    for(int i=0;i<PackageNums;++i){
+        make_command("pic",data_buffer,i);
+        memcpy(data_buffer + 1, data_c, BufferSize);
+        client.send(data_buffer,BufferSize+1);
+    }
 
-    cout << "send pic1" << endl;
-    client.send(data_1,size_1 + 1);
-    std::cin >> a;
 
-    cout << "send pic2" << endl;
-    client.send(data_2,size_1 + 1);
-    std::cin >> a;
-
-    cout << "send pic3" << endl;
-    client.send(data_3,size_3 + 1);
-    std::cin >> a;
 
     delete []data;
-    delete []data_1;
-    delete []data_2;
-    delete []data_3;
+    delete []data_buffer;
     return 0;
 }
